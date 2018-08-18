9f815250ae9519c99b26ecbd68d419382afdccda
karan <arorakaran869@gmail.com>
2018-08-18 16:08:39 +0530

    initial commit

--- app/src/main/java/com/rentsy/splash.java
+++ app/src/main/java/com/rentsy/splash.java
@@ -10,6 +10,7 @@ public class splash extends AppCompatActivity {
     static {
         System.loadLibrary("native-lib");
     }
+    //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
