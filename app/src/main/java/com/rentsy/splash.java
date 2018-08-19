850efa0273acb3a130d66987a88d5802bb90bcc3
karan <arorakaran869@gmail.com>
2018-08-19 14:53:47 +0530

    2 sec wait added on splash activity

--- app/src/main/java/com/rentsy/splash.java
+++ app/src/main/java/com/rentsy/splash.java
@@ -1,29 +1,64 @@
 package com.rentsy;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class splash extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
-
+    //this sctivity will open when user launches app
+    //this activity will ensure working data connection
+    //this will wheather user is logged in or not
+    //download pg data from server
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    class fetchalldataonsplash extends AsyncTask<String , String ,String> {
+        ProgressDialog progressDialog;
+
+
+        @Override
+        protected void onPreExecute() {
+
+            progressDialog= new ProgressDialog(splash.this);
+            progressDialog.setMessage("Loading.......");
+            progressDialog.show();
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result)
+        {
+            progressDialog.dismiss();
+            Intent i = new Intent(splash.this,login.class);
+            finish();
+            startActivity(i);
+
+            super.onPostExecute(result);
+        }
+
+    }
+
+
+
 }
